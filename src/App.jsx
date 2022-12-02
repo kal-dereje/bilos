b93e15951ac460e132d02e89c83ef8d3532c2f8e
kal-dereje <kalabdereje20@gmail.com>
2022-12-02 12:15:31 +0300

    additonal config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 function App() {
-  return <h1 className="text-red-900">hello ethiopia</h1>;
+  return (
+    <div className="h-screen w-screen ">
+      <div className="flex justify-around items-center  py-5 bg-black opacity-50 text-white w-screen">
+        <h1 className="font-Pacifico text-3xl text-Bilos-primary">
+          Bilos pastry
+        </h1>
+        <div className="flex justify-center items-center gap-12 font-Courgette font-light">
+          <h1 className="text-Bilos-primary">Home</h1>
+          <h1>About</h1>
+          <h1>Events</h1>
+          <h1>Menu</h1>
+        </div>
+        <h1>Lcoation</h1>
+      </div>
+      <p className="font-Kanit font-normal text-lg">
+        WUt qvelit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est
+        quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut.
+        Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti
+        vel. Minus et tempore modi architecto.
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Pacifico: ["Pacifico", "cursive"],
+        RobotoSlab: ["Roboto Slab", "serif"],
+        Courgette: ["Courgette", "cursive"],
+        Kanit: ["Kanit", "cursive"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        Bilos: {
+          primary: "#F6893C",
+          secondary: "#F6EC40",
+          third: "#5F5950",
+          fourth: "#CBC8C4",
+        },
+      },
+    },
   },
   plugins: [],
 };
