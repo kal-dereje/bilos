a3b6d2d7e21c91416049602ce8be446171f2d0a4
hilina-mastewal <hilina.mastewal.UE6602@gmail.com>
2022-12-21 16:41:02 +0300

    I have updated the header,Home and menu sections .in header i have  changed the location button and deleted the x cross under the list when its responsive and in the home section i have changed the 2 buttons lastly in menu section i have changed all the menu image data and the styles

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import menuList from "./MenuOption";
 import { ACTION, menuState, reducer } from "../../reducer";
 import { useReducer } from "react";
-
 function Menu() {
   const [selectedMenu, setSelectedMenu] = useState("Torta");
   const [state, dispatch] = useReducer(reducer, menuState);
@@ -14,33 +13,33 @@ function Menu() {
   return (
     <div
       id="Menu"
-      className="min-h-screen flex flex-col items-center gap-10 pt-32 bg-orange-50"
+      className="min-h-screen flex flex-col items-start gap-10 md:pt-52 pt-32 bg-[#1A1814]"
     >
-      <h1 className="text-Bilos-third font-Pacifico text-4xl text-center">
+      <h1 className="text-slate-300 font-Pacifico text-4xl md:pl-32 pl-10">
         Check our tasty <span className="text-Bilos-primary">Menu</span>
       </h1>
-      <div className="flex flex-wrap w-full sm:flex-nowrap lg:w-1/2 md:2/3 items-center gap-3 justify-center font-Kanit">
+      <div className="flex flex-wrap w-full   sm:flex-nowrap items-center gap-8 justify-center font-Kanit ">
         <button
           onClick={handelMenuClick}
           className={`${state.Torta}  ${
-            selectedMenu === "Torta" && "bg-Bilos-primary text-white"
-          } py-1 px-5 border-2 border-Bilos-primary rounded-full hover:bg-Bilos-primary hover:text-white`}
+            selectedMenu === "Torta" && "text-Bilos-primary"
+          } hover:text-Bilos-primary text-white transition-all ease-linear duration-200 hover:scale-105`}
         >
           Torta
         </button>
         <button
           onClick={handelMenuClick}
           className={`${state.Cakes}  ${
-            selectedMenu === "Cakes" && "bg-Bilos-primary text-white"
-          } py-1 px-5 border-2 border-Bilos-primary rounded-full hover:bg-Bilos-primary hover:text-white`}
+            selectedMenu === "Cakes" && "text-Bilos-primary"
+          }  hover:text-Bilos-primary text-white transition-all ease-linear duration-200 hover:scale-105`}
         >
           Cakes
         </button>
         <button
           onClick={handelMenuClick}
           className={`${state["Hot Drinks"]}  ${
-            selectedMenu === "Hot Drinks" && "bg-Bilos-primary text-white"
-          } py-1 px-5 border-2 border-Bilos-primary rounded-full hover:bg-Bilos-primary hover:text-white`}
+            selectedMenu === "Hot Drinks" && "text-Bilos-primary"
+          } hover:text-Bilos-primary text-white transition-all ease-linear duration-200 hover:scale-105`}
         >
           Hot Drinks
         </button>
@@ -48,20 +47,21 @@ function Menu() {
           href="https://qr.ultraet.com/restaurant/bilos-pastry"
           onClick={handelMenuClick}
           className={`${state["Show All"]} ${
-            selectedMenu === "Show All" && "bg-Bilos-primary text-white"
-          } py-1 px-5 border-2 border-Bilos-primary rounded-full   hover:bg-Bilos-primary hover:text-white`}
+            selectedMenu === "Show All" && "text-Bilos-primary"
+          } hover:text-Bilos-primary text-white transition-all ease-linear duration-200 hover:scale-105`}
         >
           Show All
         </a>
       </div>
-      <div className="flex flex-wrap items-center   w-full px-16 lg:px-32  gap-10">
+      <div className="grid md:grid-cols-2   grid-cols-1 w-full px-8 md:px-16 lg:px-32  gap-10">
         {menuList.map((item, index) => {
           if (selectedMenu === "Show All") {
             return (
               <Menulist
+              img={item.img}
                 menu={item.menu}
                 price={item.price}
-                descripton={item.descripton}
+                descripton={item.description}
                 key={index + 1}
               />
             );
@@ -69,9 +69,10 @@ function Menu() {
           return (
             item.type === selectedMenu && (
               <Menulist
+              img={item.img}
                 menu={item.menu}
                 price={item.price}
-                descripton={item.descripton}
+                description={item.description}
                 key={index + 1}
               />
             )
@@ -84,19 +85,25 @@ function Menu() {
 
 export default Menu;
 
-const Menulist = ({ menu, price, descripton }) => {
+const Menulist = ({img, menu, price, description }) => {
   return (
-    <div className="w-11/12 lg:w-[45%] flex  items-center gap-4 font-Kanit font-bold text-lg animate-grow ">
-      <div className="bg-[url('./assests/images/menuimages/double-chocolate-torta.jpg')] bg-cover w-16 h-16 rounded-full bg-center border-4  border-orange-200"></div>
+    <div className="w-full lg:w-[90%] flex  items-center gap-4 font-Kanit font-bold text-lg animate-grow ">
+      
+         <img
+                      src={img}
+                      className="w-16 h-16 rounded-full bg-center border-4  border-[#484643] "
+                    />
+      
 
       <div className="flex-grow">
         <div className="flex justify-between items-center w-full ">
-          <p className="text-Bilos-primary pr-1 ">{menu}</p>
+          <p className=" pr-1 text-slate-300">{menu}</p>
           <div className=" border-b border-dashed  border-Bilos-fourth flex-grow"></div>
-          <p className="text-Bilos-third pl-1">{price}</p>
+          <p className="text-Bilos-primary
+           pl-1">{price}</p>
         </div>
         <p className="text-Bilos-fourth text-sm font-extralight italic">
-          {descripton}
+          {description}
         </p>
       </div>
     </div>
